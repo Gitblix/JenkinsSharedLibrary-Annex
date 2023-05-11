a6a28722ce708b99a1ed9f4b0c4db322e8895965
mogazor <122966642+mogazor@users.noreply.github.com>
2023-05-11 20:58:40 +0000

    Update maxwell.groovy

--- vars/maxwell.groovy
+++ vars/maxwell.groovy
@@ -1,1 +1,15 @@
-
+def call(String mavenGoal){
+  
+  if ("${mavenGoal}" == "Clean")
+     {
+       sh "mvn clean package"
+     }
+  else if ("${mavenGoal}" == "Compile")
+     {
+       sh "mvn clean compile"
+     }
+  else if ("${mavenGoal}" == "Test")
+     {
+       sh "mvn clean test"
+     }
+}
